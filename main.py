77858ba552d22dc2482edec53428c58aa76b7bab
Jeremy Hines <107685330+ProfessorKaos62@users.noreply.github.com>
2023-06-19 07:09:47 -0400

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('test')
+import tkinter
+
+window = tkinter.Tk()
+window.title('First Thang')
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text='This a Label', font=('Arial', 24, 'bold'))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
